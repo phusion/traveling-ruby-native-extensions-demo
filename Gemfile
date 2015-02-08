2b69cc27af49c979bfff08a28943b0296fc8f98a
Hongli Lai (Phusion) <hongli@phusion.nl>
2015-02-08 11:42:47 +0100

    Replace 'paint' gem with 'faker' gem in preparation for Windows support

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-gem 'paint'
+gem 'faker'
 gem 'sqlite3', '1.3.9'
 
 group :development do
--- hello.rb
+++ hello.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/env ruby
-require 'paint'
+require 'faker'
 require 'sqlite3'
 
 db = SQLite3::Database.new("hello.sqlite3")
 db.execute("create table if not exists foo (name varchar(255))")
 db.execute("insert into foo values ('hello world')")
 db.close
-puts Paint["Database file modified.", :red]
+puts "Hello #{Faker::Name.name}, database file modified."
